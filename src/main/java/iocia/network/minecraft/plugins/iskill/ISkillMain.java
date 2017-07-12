c82ebf5a5af8f18e85c347421c0ff940dd369720
IociaNetwork <iocianetwork@gmail.com>
2017-07-11 19:09:26 -0600

    Base methods for main class

--- src/main/java/iocia/network/minecraft/plugins/iskill/ISkillMain.java
+++ src/main/java/iocia/network/minecraft/plugins/iskill/ISkillMain.java
@@ -3,4 +3,23 @@ package iocia.network.minecraft.plugins.iskill;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ISkillMain extends JavaPlugin {
+
+    /*---Data---*/
+
+    /*---Constructor---*/
+    public ISkillMain() {
+
+    }
+
+    /*---Methods---*/
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
 }
