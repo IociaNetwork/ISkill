565834d548539ecd46946a37bf7f9a84d5083d24
IociaNetwork <iocianetwork@gmail.com>
2017-07-11 20:44:45 -0600

    Added static instance and accessor

--- src/main/java/iocia/network/minecraft/plugins/iskill/ISkillMain.java
+++ src/main/java/iocia/network/minecraft/plugins/iskill/ISkillMain.java
@@ -1,10 +1,12 @@
 package iocia.network.minecraft.plugins.iskill;
 
+import org.apache.commons.lang.NullArgumentException;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ISkillMain extends JavaPlugin {
 
     /*---Data---*/
+    private static ISkillMain instance = null;
 
     /*---Constructor---*/
     public ISkillMain() {
@@ -14,12 +16,27 @@ public class ISkillMain extends JavaPlugin {
     /*---Methods---*/
     @Override
     public void onEnable() {
-
+        if (instance == null)
+            instance = this;
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    /**
+     * Gets the instance of the {@link ISkillMain} class.
+     * @return Instance of {@link ISkillMain} class.
+     * @throws NullArgumentException Thrown when the {@link ISkillMain} instance
+     * is null. This should almost never, and if it does happen, try a server
+     * restart, or make sure depending plugins are being initialized after
+     * this plugin.
+     */
+    public static ISkillMain getMain() throws NullArgumentException {
+        if (instance != null)
+            return instance;
+        throw new NullArgumentException("ISkill main instance is null!");
     }
 
 }
