4f452c64130de6eed7852ab149dc0a80773ee79d
IociaNetwork <iocianetwork@gmail.com>
2017-07-13 01:09:38 -0600

    Created SkillSet class

---
+++ src/main/java/iocia/network/minecraft/plugins/iskill/entities/player/SkillSet.java
@@ -0,0 +1,36 @@
+package iocia.network.minecraft.plugins.iskill.entities.player;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class SkillSet<T> {
+
+    /*---Data---*/
+    private final Map<T, EXPEntity> skills;
+
+    /*---Constructors---*/
+    private SkillSet(Map<T, EXPEntity> skills) {
+        this.skills = skills;
+    }
+
+    /*---Methods---*/
+    public EXPEntity get(T id) {
+        return skills.get(id);
+    }
+
+    /*---Builder---*/
+    public static class Builder<TKey> {
+        private Map<TKey, EXPEntity> skills = new HashMap<>();
+
+        public Builder addSkill(TKey skillId, EXPEntity entity) {
+            if (skills.containsKey(skillId))
+                throw new IllegalArgumentException("A skill with the same Id has already been to the skill set");
+            skills.put(skillId, entity);
+            return this;
+        }
+
+        public SkillSet build() {
+            return new SkillSet<>(skills);
+        }
+    }
+}
